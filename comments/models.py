595e73513f27b555f446e7da23e4d696065f109e
chienchuanw <chienchuanwww@gmail.com>
2024-12-10 17:35:48 +0800

    add: create model Comment

---
+++ comments/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=20)
+    content = models.TextField(blank=True, null=True)
