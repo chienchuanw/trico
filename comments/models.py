b92807293445d6476d5f2e6a3d2da00e6faf4ac3
chienchuanw <chienchuanwww@gmail.com>
2024-12-10 23:13:28 +0800

    delete: remove comments-releted files

--- comments/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class Comment(models.Model):
-    author = models.CharField(max_length=20)
-    content = models.TextField(blank=True, null=True)
-    like = models.BooleanField(default=False)
